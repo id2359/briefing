416c6b3da6cc1070253c7dc1b612c7a77170b0f4
Crat <id2359@yahoo.com>
2015-01-20 21:52:09 +0800

    analysis interface

---
+++ analyse.mli
@@ -0,0 +1,4 @@
+type analysis
+val write_facts : Collect.reading list -> unit (* write readings to file *)
+val run_analysis : string * string -> analysis (* rule file , fact file -> run clips and create analysis *)
+ 
