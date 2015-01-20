f61abd4379b5cbccba0b90b48fefc9d6e9481b26
Crat <id2359@yahoo.com>
2015-01-20 22:05:17 +0800

    curried run_analysis

--- analyse.mli
+++ analyse.mli
@@ -1,4 +1,4 @@
 type analysis
 val write_facts : Collect.reading list -> string  -> unit (* write readings to file *)
-val run_analysis : string * string -> analysis (* rule file , fact file -> run clips and create analysis *)
+val run_analysis : string ->  string -> analysis (* rule file , fact file -> run clips and create analysis *)
  
