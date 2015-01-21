2a4d0dc0076d3bfe3189f49c81ba7b654f17bf5a
Crat <id2359@yahoo.com>
2015-01-22 06:47:23 +0800

    added parser module mli

--- collect.ml
+++ collect.ml
@@ -1,1 +1,11 @@
-type t = R of string * float
+type reading = Rfloat of string * float
+	       | Rint of string * int
+	       | Rstring of string * string
+
+let read_url url reading_parser =
+  try
+    let html = Web.get url in
+    reading_parser html
+  with
+      Web.exception -> "ERROR"
+
--- collect.mli
+++ collect.mli
@@ -1,3 +1,4 @@
 type reading        (** a datum grabbed from the web - eg tomorrows max temperature *)
-val read_url : string -> reading (** hmm this is wrong *)
+
+val read_url : string -> Parser.t -> reading
 val read_url_file : string -> reading list (** ditto - change *)
