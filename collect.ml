019b5ef953d4c73f117a6e5b2c1e8af65bde5066
Crat <id2359@yahoo.com>
2015-01-28 06:33:25 +0800

    interface change

--- collect.ml
+++ collect.ml
@@ -1,13 +1,11 @@
-open Printf;;
-
 type reading = Rfloat of string * float
 	       | Rint of string * int
 	       | Rstring of string * string;;
 
 let print_reading r = match r with
-  | Rfloat (tag, value) -> print_endline tag ^ "=" ^  string_of_float value
-  | Rint (tag, value) -> print_endline tag ^ "=" ^ string_of_int value
-  | Rstring (tag, value) -> print_endline tag ^ "=" ^ value
+  | Rfloat (tag, value) -> print_endline ( tag ^ "=" ^  string_of_float value )
+  | Rint (tag, value) -> print_endline ( tag ^ "=" ^ string_of_int value )
+  | Rstring (tag, value) -> print_endline ( tag ^ "=" ^ value )
 
 let collect_weather html =
   [ Rfloat ("weather", 23.65)];;
--- collect.mli
+++ collect.mli
@@ -2,4 +2,6 @@ type reading = Rfloat of string * float
 	       | Rint of string * int
 	       | Rstring of string * string;;
 
+
+val print_reading : reading -> unit
 val collect : string -> string -> reading list  (** takes tag to dispatch on and parses html string *)
