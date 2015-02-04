9ffdfb08f29c31eb3d8c98b228a5f7f29549e72e
Crat <id2359@yahoo.com>
2015-02-05 06:46:39 +0800

    changed function invocation for main

--- briefing.ml
+++ briefing.ml
@@ -1,6 +1,6 @@
 open Printf;;
 (* ocamlfind ocamlc -package curl -linkpkg web.ml` collect.ml briefing.ml -o briefing *)
-let () =
+let _ =
   let html = Web.get Sys.argv.(1) in
   let reading_tag = Sys.argv.(2) in   
   let readings = Collect.collect reading_tag html in
